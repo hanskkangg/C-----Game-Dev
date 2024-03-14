fb6f67f9c7286126e2652cfa07cd1ce7fe79fc9b
hanskkangg <hans.kkang@gmail.com>
2024-03-13 23:55:25 -0400

    first commit

---
+++ .vscode/TripleX.cpp
@@ -0,0 +1,13 @@
+//it's like importing iostream in java.
+#include <iostream>
+
+
+//creating main() class and int is a return type.
+int main()
+{
+    //In Java, the equivalent would be System.out.println("Hello, World!");
+    std::cout << "You are a secrete agent breaking into a secure server room.";
+    std::cout <<"Enter the correct code to continue....";
+    return 0;
+}   
+
---
+++ TripleX.cpp
@@ -0,0 +1,14 @@
+//it's like importing iostream in java.
+#include <iostream>
+
+
+//creating main() class and int is a return type.
+int main()
+{
+    //sdt::cout = In Java, the equivalent would be System.out.println("Hello, World!");
+    std::cout << "You are a secrete agent breaking into a secure server room.";
+    std::cout <<"Enter the correct code to continue....";
+    return 0;
+}   
+
+
