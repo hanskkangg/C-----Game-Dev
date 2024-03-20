899fb4339cef853868e9fab26ac8bf6137d88909
hanskkangg <hans.kkang@gmail.com>
2024-03-20 13:32:38 -0400

    Fifth Commit - Generated random numbers for each variable using modulo + 1, and set the maximum difficulty level to 5. The difficulty of each level increases incrementally. Implemented the srand(time(NULL)) function to create a new random sequence based on the time of day.

--- .vscode/TripleX.cpp
+++ .vscode/TripleX.cpp
@@ -1,24 +1,39 @@
+/* 
+Programmer's Comments: Tasks to complete include adding an array list of items (Weapon, Gloves, Boots, Armor, Helmet) and assigning them to the player as they progress through levels.
+
+Change the setting as the player progresses through levels.
+
+The first village corresponds to the first level.
+The second village corresponds to the second level.
+And so on...
+By the end of the levels, the player will be fully equipped to start their journey.
+
+Additionally, modify the narrator's script accordingly.
+*/
+
 //it's like importing iostream in java. it's called Preprocessor Directive.
 #include <iostream>
+#include <ctime>
 
-void PrintIntro()
+void PrintIntro(int Difficulty)
 {
 std::cout << std::endl;
 //std::cout << = In Java, the equivalent would be System.out.println("Hello, World!");
-std::cout << "\n\nYou're a goblin, deprived, owning nothing, wandering the labyrinthine depths of the dungeon.\n\n";
+std::cout << "\n\nYou're a goblin, deprived, owning nothing, wandering the labyrinthine depths of the dungeon. Level: " << Difficulty << "\n\n";
 std::cout << "In a secluded chamber, illuminated by the flickering light of a single fire, you stand before the chieftain's tipi. The air inside is thick with the scent of ancient incense.\n";
 std::cout << "As you push aside the hide covering the entrance, you find the chieftain sitting cross-legged on the ground, surrounded by totems and mystical symbols. His skeletal frame complements an expression filled with anger, sorrow, and a profound sense of emptiness. He says,\n\n";
 std::cout << "\"You're merely a goblin, outwitted even by slimes, and with such foolishness, your chances against the heavily shielded men of flesh are nonexistent. Prove to me your worth.\"\n";
 }
 
-bool PlayGame()
+bool PlayGame(int Difficulty)
 {
-    PrintIntro();
+    PrintIntro(Difficulty);
 
-    //declaring 3 variables
-    const int NumA = 4;
-    const int NumB = 2;
-    const int NumC = 1;
+    // Declaring 3 variables. I can randomize the numbers by using the modulo operator. This will generate random numbers between 0 and 3.
+    const int NumA = rand() % Difficulty + Difficulty + 1 ;
+    const int NumB = rand() % Difficulty + Difficulty + 1;
+    const int NumC = rand() % Difficulty + Difficulty + 1;
+    //By using this formula, const int NumC = rand() % Difficulty + Difficulty + 1;, we can increase the difficulty as the game progresses.
 
     //const in c++ is like final in java
     const int NumSum = NumA + NumB + NumC;
@@ -43,6 +58,7 @@ bool PlayGame()
     Scanner scanner = new Scanner(System.in);
     int PlayerGuess = scanner.nextInt();
     */
+   
     // Read three guesses from the player
     std::cin >> GuessA >> GuessB >> GuessC;
 
@@ -71,15 +87,26 @@ creating main() class and int is a return type.
 */
 int main()
 {
+    srand(time(NULL)); // create new random sequence based on time of day
 
-    //implemented while loop so that player can play again.
-    while(true)
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop the game until all leves completed
     {
-    bool bLevelComplete = PlayGame();
-    PlayGame();
-    std::cin.clear();
-    std::cin.ignore();
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // clear any errors
+        std::cin.ignore(); // Discards the buffer
+
+
+        //increase the level difficulty 
+        if (bLevelComplete) 
+        {
+            ++LevelDifficulty;
+        }
     }
+    
+    std::cout << "Now you're well equipped...go hunt!";
     return 0;
 }   
 
